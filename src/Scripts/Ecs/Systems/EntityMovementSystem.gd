692e13984e888a02ff70393977ea95d4d6d3b10e
ala <aladdinalkhafaji@gmail.com>
2019-12-22 21:33:50 +0300

    Requested: remove unnecessary list

--- src/Scripts/Ecs/Systems/EntityMovementSystem.gd
+++ src/Scripts/Ecs/Systems/EntityMovementSystem.gd
@@ -5,12 +5,8 @@ func on_move_entity(entity, x, y):
 		entity.x = clamp(x, 0, Constants.TILES_WIDE - 1)
 		entity.y = clamp(y, 0, Constants.TILES_HIGH - 1)
 
-func get_entities_on(x, y):
-	var ret = []
+func is_empty(x, y):
 	for e in entities:
 		if e.x == x and e.y == y:
-			ret.append(e)
-	return ret
-
-func is_empty(x, y):
-	return len(get_entities_on(x, y)) == 0
+			return false
+	return true
