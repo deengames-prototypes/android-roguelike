4e86bbf7f4de7dcdea2d09d0ac6025f3afc35f16
ala <aladdinalkhafaji@gmail.com>
2019-12-22 20:23:12 +0300

    Walls no longer walkable

--- src/Scripts/Ecs/Systems/EntityMovementSystem.gd
+++ src/Scripts/Ecs/Systems/EntityMovementSystem.gd
@@ -1,5 +1,16 @@
 extends "res://Scripts/Ecs/Core/System.gd"
 
 func on_move_entity(entity, x, y):
-	entity.x = clamp(x, 0, Constants.TILES_WIDE - 1)
-	entity.y = clamp(y, 0, Constants.TILES_HIGH - 1)
+	if is_empty(x, y):
+		entity.x = clamp(x, 0, Constants.TILES_WIDE - 1)
+		entity.y = clamp(y, 0, Constants.TILES_HIGH - 1)
+
+func get_entities_on(x, y):
+	var ret = []
+	for e in entities:
+		if e.x == x and e.y == y:
+			ret.append(e)
+	return ret
+
+func is_empty(x, y):
+	return len(get_entities_on(x, y)) == 0
