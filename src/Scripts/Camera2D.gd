84ac5a89dc197f7ce15a224960b33179a367d62e
ala <aladdinalkhafaji@gmail.com>
2019-12-22 21:26:35 +0300

    Camera limited by map size

--- src/Scripts/Camera2D.gd
+++ src/Scripts/Camera2D.gd
@@ -1,4 +1,11 @@
 extends Camera2D
 
+# TODO: extract magic numbers
+func _ready():
+	limit_top = 0
+	limit_left = 0
+	limit_right = Constants.TILES_WIDE * 32
+	limit_bottom = Constants.TILES_HIGH * 32
+
 func center_on(entity):
 	global_position = Vector2(entity.x * 32, entity.y * 32)
