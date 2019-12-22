543c4cc1148b63cafd1258cd1e4fb4511bbc6430
ala <aladdinalkhafaji@gmail.com>
2019-12-22 12:10:13 +0300

    Clean up DisplaySystem

--- src/Scripts/Ecs/Systems/DisplaySystem.gd
+++ src/Scripts/Ecs/Systems/DisplaySystem.gd
@@ -3,26 +3,25 @@ extends "res://Scripts/Ecs/Core/System.gd"
 var _tiles_tilemap:TileMap
 var _entities_tilemap:TileMap
 
-# TODO: figure this out
-var tile_name_to_index = {
-	"Wall": 0,
-	"Player": 0
-}
+var tilemaps_by_name = {}
 
 func _init(tiles_tilemap:TileMap, entities_tilemap:TileMap):
 	self.required_component_types = ["SpriteComponent"]
 	_tiles_tilemap = tiles_tilemap;
 	_entities_tilemap = entities_tilemap
 	
+	tilemaps_by_name = {
+		"Ground": _tiles_tilemap,
+		"Creatures": _entities_tilemap
+	}
+
 	_tiles_tilemap.clear()
 	_entities_tilemap.clear()
 
 func on_update():
 	for entity in self.entities:
 		var component = entity.get("SpriteComponent")
-		var tile_index = tile_name_to_index[component.tile_name]
-		
-		if component.layer == "Ground":
-			_tiles_tilemap.set_cell(entity.x, entity.y, tile_index)
-		elif component.layer == "Creatures":
-			_entities_tilemap.set_cell(entity.x, entity.y, tile_index)
+
+		var tilemap = tilemaps_by_name[component.layer]
+		var tile_index = tilemap.tile_set.find_tile_by_name(component.tile_name)
+		tilemap.set_cell(entity.x, entity.y, tile_index)
