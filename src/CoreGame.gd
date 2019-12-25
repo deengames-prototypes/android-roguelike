7c8fb79d33dc1818c0df5e4e43785e0e31048284
nightblade9 <nightbladecodes@gmail.com>
2019-12-24 19:15:27 -0500

    Refactor: remove SystemGroup TSCN `SystemGroup.gd` creates where necessary, keeps track of things from `CoreGame`

--- src/CoreGame.gd
+++ src/CoreGame.gd
@@ -1,12 +1,15 @@
 extends Node2D
 
+const SystemGroup = preload("res://Scripts/Ecs/Extension/SystemGroup.gd")
+
 var _dungeon_generator = preload("res://Scripts/DungeonGenerator.gd").new()
 var _event_bus = preload("res://Scripts/EventBus.gd").new()
-onready var _system_group = $SystemGroup
+var _system_group:SystemGroup
 
 func _ready():
+	_system_group = SystemGroup.new($Ground, $Creatures, $Camera2D)
 	_system_group._setup(_event_bus)
-	_dungeon_generator.generate_dungeon($SystemGroup/Ground, _event_bus)
+	_dungeon_generator.generate_dungeon(_system_group.get_ground(), _event_bus)
 
 func _process(delta):
 	_system_group.update_systems()
--- src/Scripts/Ecs/Extension/SystemGroup.gd
+++ src/Scripts/Ecs/Extension/SystemGroup.gd
@@ -5,24 +5,16 @@ const PlayerMovementSystem = preload("res://Scripts/Ecs/Systems/PlayerMovementSy
 const EntityMovementSystem = preload("res://Scripts/Ecs/Systems/EntityMovementSystem.gd")
 const CameraSystem = preload("res://Scripts/Ecs/Systems/CameraSystem.gd")
 
+var _ground_tilemap:TileMap
+var _creatures_tilemap:TileMap
+var _camera:Camera2D
+
 var _systems:Array = []
 
-func _setup(event_bus):
-	_setup_tilemaps()
-	_setup_systems(event_bus)
-	
-	event_bus.connect("spawn_entity", self, "add_entity")
-
-func _setup_tilemaps():
-	var cell_size = Vector2(Constants.TILE_WIDTH, Constants.TILE_HEIGHT)
-	$Ground.cell_size = cell_size
-	$Creatures.cell_size = cell_size
-
-func _setup_systems(event_bus):
-	_systems.append(DisplaySystem.new($Ground, $Creatures))
-	_systems.append(PlayerMovementSystem.new(event_bus))
-	_systems.append(EntityMovementSystem.new(event_bus))
-	_systems.append(CameraSystem.new($Camera2D))
+func _init(ground_tilemap, creatures_tilemap, camera):
+	_ground_tilemap = ground_tilemap
+	_creatures_tilemap = creatures_tilemap
+	_camera = camera
 
 func add_entity(e):
 	for system in _systems:
@@ -31,3 +23,22 @@ func add_entity(e):
 func update_systems():
 	for system in _systems:
 		system.on_update()
+
+func get_ground():
+	return _ground_tilemap
+		
+func _setup(event_bus):
+	_setup_tilemaps()
+	_setup_systems(event_bus)
+	event_bus.connect("spawn_entity", self, "add_entity")
+
+func _setup_tilemaps():
+	var cell_size = Vector2(Constants.TILE_WIDTH, Constants.TILE_HEIGHT)
+	_ground_tilemap.cell_size = cell_size
+	_creatures_tilemap.cell_size = cell_size
+
+func _setup_systems(event_bus):
+	_systems.append(DisplaySystem.new(_ground_tilemap, _creatures_tilemap))
+	_systems.append(PlayerMovementSystem.new(event_bus))
+	_systems.append(EntityMovementSystem.new(event_bus))
+	_systems.append(CameraSystem.new(_camera))
