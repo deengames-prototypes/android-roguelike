e87ec238aa2dabaa5bd5bd59d16dddbccec9b1c9
nightblade9 <nightbladecodes@gmail.com>
2019-12-28 20:11:35 -0500

    Add `Features` auto-load script with sample values

---
+++ src/Scripts/Features.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+# A list of feature toggles. Setting a value of "true" turns a feature
+# on, and setting a value of "false" turns a feature off.
+
+const SHIELD_DIES_AFTER_3_HITS = false
+const SHIELD_DIES_AFTER_20_STEPS = false
