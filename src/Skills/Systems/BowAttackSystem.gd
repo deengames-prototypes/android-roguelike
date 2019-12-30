a327dfc40a0fff29c3d6f80b285a7fa2865ebad0
ala <aladdinalkhafaji@gmail.com>
2019-12-30 22:11:15 +0300

    Requested: refactor condition for clarity

--- src/Skills/Systems/BowAttackSystem.gd
+++ src/Skills/Systems/BowAttackSystem.gd
@@ -10,7 +10,7 @@ func _init(event_bus):
 func activate(source, target: Vector2):
 	# TODO: ensure line of sight
 	var target_entity = _get_entity_at(target)
-	if target_entity != null and not source == target_entity:
+	if target_entity != null and source != target_entity:
 		var damage = Constants.BOW_BASE_DAMAGE
 		var distance = int(source.position.distance_to(target)) - 1
 		damage -= Constants.BOW_DAMAGE_LOSS_PER_DISTANCE_UNIT * distance
