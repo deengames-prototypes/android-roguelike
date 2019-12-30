99da837d21e58ce55e4ba4a31fa1113f9bb32a52
ala <aladdinalkhafaji@gmail.com>
2019-12-30 22:23:48 +0300

    Requested: Add comment explaining obscure code

--- src/Ecs/Systems/ActivateTileSystem.gd
+++ src/Ecs/Systems/ActivateTileSystem.gd
@@ -9,6 +9,8 @@ func _init(event_bus, tilemap):
 
 func _input(event):
 	if event is InputEventScreenTouch and event.pressed == true:
+		# xform_inv translates touch position from position in viewport to global position
+		# see: https://godotengine.org/qa/28543/godot-3-0-2-get-global-position-from-touchscreen
 		_activate_tile(get_canvas_transform().xform_inv(event.position))
 	elif event is InputEventMouseButton and event.pressed == true and event.button_index == BUTTON_LEFT:
 		_activate_tile(get_global_mouse_position())
