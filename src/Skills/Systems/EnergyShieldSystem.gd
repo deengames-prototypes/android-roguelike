f00d9056495d179af5700cf19c2d65ad0501ccdc
ala <aladdinalkhafaji@gmail.com>
2020-01-01 21:22:34 +0300

    Fix indentation

--- src/Skills/Systems/EnergyShieldSystem.gd
+++ src/Skills/Systems/EnergyShieldSystem.gd
@@ -4,22 +4,22 @@ const EnergyShieldEffect = preload("res://Effects/Damage/EnergyShieldEffect.gd")
 
 func _init(event_bus):
 	required_component_types = ["StatusEffectsComponent"]
-    event_bus.connect("energy_shield", self, 'activate')
+	event_bus.connect("energy_shield", self, 'activate')
 
 func activate(entity):
-    var status_effects_component = entity.get("StatusEffectsComponent")
-    var effects = status_effects_component.effects
+	var status_effects_component = entity.get("StatusEffectsComponent")
+	var effects = status_effects_component.effects
 
-    # if there is an existing shield, reset its strength
-    var shield = _get_shield(effects)
-    if shield != null:
-        shield.strength = Constants.ENERGY_SHIELD_STRENGTH
-    else:
-        shield = EnergyShieldEffect.new(status_effects_component, Constants.ENERGY_SHIELD_STRENGTH)
-        effects.add(shield)
+	# if there is an existing shield, reset its strength
+	var shield = _get_shield(effects)
+	if shield != null:
+		shield.strength = Constants.ENERGY_SHIELD_STRENGTH
+	else:
+		shield = EnergyShieldEffect.new(status_effects_component, Constants.ENERGY_SHIELD_STRENGTH)
+		effects.add(shield)
 
 func _get_shield(effects):
-    for e in effects:
-        if e is EnergyShieldEffect:
-            return e
+	for e in effects:
+		if e is EnergyShieldEffect:
+			return e
 	return null
