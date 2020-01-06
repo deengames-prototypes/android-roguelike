dd62aa9c92666519dd290583f679a11d695806ac
ala <aladdinalkhafaji@gmail.com>
2020-01-07 00:08:18 +0300

    All playable entites add to collective FOV

--- src/Ecs/Systems/FovSystem.gd
+++ src/Ecs/Systems/FovSystem.gd
@@ -2,7 +2,7 @@ extends "res://Ecs/Core/System.gd"
 
 var _event_bus
 var _tilemap
-var _player = null
+var fov_map = {}  # playable_entity: fov
 
 var fov_lib = preload("res://Scripts/fov.gd").new()
 
@@ -11,21 +11,25 @@ func _init(event_bus, ground_tilemap):
 	_tilemap = ground_tilemap
 
 	_event_bus.connect("move_entity", self, "on_move_entity")
-	_event_bus.connect("set_player", self, "on_set_player")
+	_event_bus.connect("spawn_entity", self, "on_move_entity")
 
-func on_set_player(player):
-	_player = player
-	update_player_fov(_player.position)
-
-func on_move_entity(entity, new_position):
-	if _player != null and entity == _player and _is_empty(new_position):
-		update_player_fov(new_position)
-
-func update_player_fov(pos):
-	_event_bus.emit_signal("fov_change", calculate_fov_from(pos, _player.get("SightComponent").sight_radius))
+func on_move_entity(entity, new_position=null):
+	if new_position == null or not _is_empty(new_position):
+		new_position = entity.position
+	
+	if entity.has("PlayableComponent") and entity.has("SightComponent"):
+		fov_map[entity] = calculate_fov_from(new_position, entity.get("SightComponent").sight_radius)
+		_event_bus.emit_signal("fov_change", get_total_fov())
 
 func calculate_fov_from(pos, radius):
 	return fov_lib.do_fov(_tilemap, pos, radius)
+
+func get_total_fov():
+	var total_fov = {}
+	for fov in fov_map.values():
+		for tile in fov:
+			total_fov[tile] = true
+	return total_fov
 
 func _is_empty(pos):
 	for e in entities:
